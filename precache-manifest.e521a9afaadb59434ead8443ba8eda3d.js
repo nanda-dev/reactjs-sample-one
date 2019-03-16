9c42905313348bef7cda347571aacdcdaed237af
Nanda.GS <nandagopan.gs@cognizant.com>
2019-03-16 23:01:05 +0530

    Updates

---
+++ precache-manifest.e521a9afaadb59434ead8443ba8eda3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "52226cbfcac25b68aca3",
+    "url": "/react-tutorial/static/js/main.52226cbf.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/react-tutorial/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "52226cbfcac25b68aca3",
+    "url": "/react-tutorial/static/css/main.22261310.chunk.css"
+  },
+  {
+    "revision": "802edcacf94fbf0488beeebaa442d9c3",
+    "url": "/react-tutorial/index.html"
+  }
+];
